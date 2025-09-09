8b272f4a13c55d090ad434eb56bd26092d3881ce
João Victor <joao52304@outlook.com.br>
2025-09-09 18:08:44 -0300

    Primeiro commiot

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone nokia = new Nokia("587226", "Asus", "222222222", 256);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+            Console.WriteLine("\n");
+
+            Smartphone iphone = new Iphone("241358","Iphone 18"," 1111111111111",458);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+
+        }
+    }
+}
